c9fd41c8d8c6b15602b33817e79e51c4841ba12e
Janne K. Olesen <janne.olesen@oberbaum-concept.com>
2016-05-18 23:38:18 +0200

    junit tests improved / added

--- src/test/java/net/obecon/properties/internal/PropertyGettersTest.java
+++ src/test/java/net/obecon/properties/internal/PropertyGettersTest.java
@@ -16,7 +16,12 @@
 
 package net.obecon.properties.internal;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.lang.reflect.Method;
+import java.util.Base64;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -25,6 +30,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import net.obecon.properties.MissingPropertyException;
 import net.obecon.properties.PropertyParseException;
 import net.obecon.properties.converter.BooleanConverter;
 import net.obecon.properties.converter.CharConverter;
@@ -33,6 +39,7 @@ import net.obecon.properties.converter.DoubleConverter;
 import net.obecon.properties.converter.FloatConverter;
 import net.obecon.properties.converter.IntConverter;
 import net.obecon.properties.converter.LongConverter;
+import net.obecon.properties.converter.ParseException;
 import net.obecon.properties.converter.ShortConverter;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -65,16 +72,51 @@ public class PropertyGettersTest {
 	}
 
 
-	private Object[] parameters() {
-		return new Object[]{
-				new GetAsTestCase("getAsChar", CharConverter.INSTANCE, "c", 'c', 'a', char.class),
-				new GetAsTestCase("getAsBoolean", BooleanConverter.INSTANCE, "true", true, false, boolean.class),
-				new GetAsTestCase("getAsShort", ShortConverter.INSTANCE, "1", (short) 1, (short) 0, short.class),
-				new GetAsTestCase("getAsInt", IntConverter.INSTANCE, "1", 1, 0, int.class),
-				new GetAsTestCase("getAsLong", LongConverter.INSTANCE, "1", 1L, 0L, long.class),
-				new GetAsTestCase("getAsFloat", FloatConverter.INSTANCE, "1", 1f, 0f, float.class),
-				new GetAsTestCase("getAsDouble", DoubleConverter.INSTANCE, "1", 1.0, 1.0, double.class),
-		};
+	@Test
+	public void get() throws Exception {
+		final SerializableObject expected = new SerializableObject("expected");
+		when(propertyGetters.hasKey(KEY)).thenReturn(true);
+		when(propertyGetters.getAsString(KEY)).thenReturn(serialize(expected));
+		assertEquals(expected, propertyGetters.get(KEY));
+	}
+
+
+	@Test
+	public void get_MissingPropertyException() throws Exception {
+		when(propertyGetters.getAsString(KEY)).thenThrow(new MissingPropertyException(KEY));
+		thrown.expect(MissingPropertyException.class);
+		thrown.expectMessage("missing property '" + KEY + "'");
+		propertyGetters.get(KEY);
+
+	}
+
+
+	@Test
+	public void get_PropertyParseException() throws Exception {
+		final SerializableObject expected = new SerializableObject("expected");
+		when(propertyGetters.hasKey(KEY)).thenReturn(true);
+		when(propertyGetters.getAsString(KEY)).thenReturn("broken" + serialize(expected));
+		thrown.expect(PropertyParseException.class);
+		thrown.expectMessage("failed to deserialize property '" + KEY + "'");
+		propertyGetters.get(KEY);
+	}
+
+
+	@Test
+	public void get_withDefault_getProperty() throws Exception {
+		final SerializableObject expected = new SerializableObject("expected");
+		final SerializableObject defaultValue = new SerializableObject("default");
+		when(propertyGetters.hasKey(KEY)).thenReturn(true);
+		when(propertyGetters.getAsString(KEY)).thenReturn(serialize(expected));
+		assertEquals(expected, propertyGetters.get(KEY, defaultValue));
+	}
+
+
+	@Test
+	public void get_withDefault_getDefault() throws Exception {
+		final SerializableObject expected = new SerializableObject("expected");
+		when(propertyGetters.hasKey(KEY)).thenReturn(false);
+		assertEquals(expected, propertyGetters.get(KEY, expected));
 	}
 
 
@@ -96,17 +138,35 @@ public class PropertyGettersTest {
 
 
 	@Test
-	public void get_ParseException() throws Throwable {
+	public void get_Converter_ParseException() throws Throwable {
+		Converter converter = mock(Converter.class);
+		when(converter.fromString(anyString())).thenThrow(new ParseException(""));
+		when(converter.getTargetClass()).thenReturn(String.class);
+		final String value = "string";
+		when(propertyGetters.getAsString(KEY)).thenReturn(value);
 		thrown.expect(PropertyParseException.class);
-		thrown.expectMessage(startsWith("failed to parse property 'key'='string' to java.lang.Boolean"));
-		when(propertyGetters.getAsString(KEY)).thenReturn("string");
-		propertyGetters.get(KEY, BooleanConverter.INSTANCE);
+		thrown.expectMessage(
+				String.format("failed to parse property '%s'='%s' to %s", KEY, value, String.class.getName()));
+		propertyGetters.get(KEY, converter);
+	}
+
+
+	private Object[] parameters() {
+		return new Object[]{
+				new GetAsTestCase("getAsChar", CharConverter.INSTANCE, "c", 'c', 'a', char.class),
+				new GetAsTestCase("getAsBoolean", BooleanConverter.INSTANCE, "true", true, false, boolean.class),
+				new GetAsTestCase("getAsShort", ShortConverter.INSTANCE, "1", (short) 1, (short) 0, short.class),
+				new GetAsTestCase("getAsInt", IntConverter.INSTANCE, "1", 1, 0, int.class),
+				new GetAsTestCase("getAsLong", LongConverter.INSTANCE, "1", 1L, 0L, long.class),
+				new GetAsTestCase("getAsFloat", FloatConverter.INSTANCE, "1", 1f, 0f, float.class),
+				new GetAsTestCase("getAsDouble", DoubleConverter.INSTANCE, "1", 1.0, 1.0, double.class)
+		};
 	}
 
 
 	@Parameters(method = "parameters")
 	@Test
-	public void get(GetAsTestCase testCase) throws Throwable {
+	public void get_Converter(GetAsTestCase testCase) throws Throwable {
 		when(propertyGetters.getAsString(KEY)).thenReturn(testCase.value);
 		Object result = propertyGetters.get(KEY, testCase.converter);
 		assertEquals(testCase.expected, result);
@@ -115,7 +175,7 @@ public class PropertyGettersTest {
 
 	@Parameters(method = "parameters")
 	@Test
-	public void get_withDefault_getProperty(GetAsTestCase testCase) throws Throwable {
+	public void get_Converter_withDefault_getProperty(GetAsTestCase testCase) throws Throwable {
 		when(propertyGetters.getAsString(KEY)).thenReturn(testCase.value);
 		Object result = propertyGetters.get(KEY, testCase.converter);
 		assertEquals(testCase.expected, result);
@@ -124,7 +184,7 @@ public class PropertyGettersTest {
 
 	@Parameters(method = "parameters")
 	@Test
-	public void get_withDefault_getDefault(GetAsTestCase testCase) throws Throwable {
+	public void get_Converter_withDefault_getDefault(GetAsTestCase testCase) throws Throwable {
 		when(propertyGetters.hasKey(KEY)).thenReturn(false);
 		Object result = propertyGetters.get(KEY, testCase.converter, testCase.defaultValue);
 		verify(propertyGetters).hasKey(eq(KEY));
@@ -169,6 +229,16 @@ public class PropertyGettersTest {
 	}
 
 
+	private String serialize(SerializableObject object) throws IOException {
+		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try (final ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+			oos.writeObject(object);
+			oos.flush();
+			return Base64.getEncoder().encodeToString(bos.toByteArray());
+		}
+	}
+
+
 	static class GetAsTestCase<T> {
 
 		final String methodName;
@@ -192,6 +262,35 @@ public class PropertyGettersTest {
 		@Override
 		public String toString() {
 			return methodName;
+		}
+	}
+
+	static class SerializableObject implements Serializable {
+
+		private final String value;
+
+
+		public SerializableObject(String value) {
+			this.value = value;
+		}
+
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+			SerializableObject serializableObject = (SerializableObject) o;
+			return value.equals(serializableObject.value);
+		}
+
+
+		@Override
+		public int hashCode() {
+			return value.hashCode();
 		}
 	}
 }
